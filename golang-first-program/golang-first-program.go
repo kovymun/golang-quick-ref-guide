826f7abdc1723d99ee943991dd02292681238c16
Kovy <codingstudent2021@gmail.com>
2024-09-22 23:00:18 +0200

    Adding GoLang variable concepts

---
+++ golang-first-program/golang-first-program.go
@@ -0,0 +1,15 @@
+/*
+* GoLang Program Entry Point
+* We are going to write our first GoLang Program, the classic "Hello Word"
+
+ */
+
+package main //what is this?
+
+import "fmt" //what is this?
+
+func main() {
+	fmt.Println("Hello World")
+}
+
+//Running the program: go run golang-first-program.go
