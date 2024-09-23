6139903b10131ccd38fca0a01c6f1613e3f35226
Kovy <codingstudent2021@gmail.com>
2024-09-23 21:14:26 +0200

    Adding Go functions

--- golang-data-types/golang-data-types.go
+++ golang-data-types/golang-data-types.go
@@ -3,7 +3,7 @@ package main
 import "fmt"
 
 func main() {
-	fName := "James" //inferred as a
+	fName := "James" //inferred as type string
 	var age int = 10 //string type integer
 	fmt.Println(fName, age)
 }
@@ -12,7 +12,7 @@ func main() {
 --------------
 GO DATA-TYPES:
 --------------
-* DATA-TYPES USED IN GO: string, int, bool, float64, array
+* DATA-TYPES USED IN GO: string | int | bool | float64 | array
 * SYNTAX: var *identifier-name* *data-type* = initialized value
 * EXAMPLE: var fName string = "James"
 * IMPORTANT: When you declare a variable using the := syntax, Go automatically infers the type based on the assignment value.
---
+++ golang-functions/golang-functions.go
@@ -0,0 +1,25 @@
+package main
+
+import "fmt"
+
+func main() {
+	//printing output to the console.
+	fmt.Println(names())
+}
+
+// Syntax for a go lang function
+func names() string {
+	return "This is my name"
+}
+
+/*
+------
+NOTES:
+------
+
+* What is a function? A function is small reusable piece of code that contains a specific action/logic.
+* SYNTAX:
+	function functionName() data-type {
+		//function body logic
+	}
+*/
