ca556b159b13c591ba041127cfb0a8e4f6d9d002
Kovy <codingstudent2021@gmail.com>
2024-12-12 11:13:56 +0200

    Adding go struct concepts

---
+++ golang-structs/golang-structs.go
@@ -0,0 +1,24 @@
+package main
+
+func main() {
+
+}
+
+/*
+------
+NOTES:
+------
+* What is an Struct in Go?
+  - A struct is a composite data type in Go, short for "structure."
+  - It allows you to group related properties (fields) into a single unit, making your code more organized and readable.
+  - If you're familiar with JavaScript, a struct in Go is conceptually similar to an "object" in JavaScript.
+  - It is useful when you want to represent real-world entities or models with multiple properties.
+
+* SYNTAX:
+  - type StructName struct {
+        FieldName DataType
+        FieldName DataType
+    }
+
+
+*/
